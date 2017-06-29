6b19efb55a63544fe2df988461515f13f3f2d886
Neioumi <neioumi@me.com>
2017-06-29 20:24:25 +0900

    Unityちゃんを左右移動できるように

--- Assets/Scripts/UnityChanController.cs
+++ Assets/Scripts/UnityChanController.cs
@@ -9,6 +9,10 @@ public class UnityChanController : MonoBehaviour {
 	private Rigidbody myRigidbody;
 	// 前進のための力
 	private float forwardForce = 800.0f;
+	// 左右移動のための力
+	private float turnForce = 500.0f;
+	// 左右移動できる範囲
+	private float movableRange = 3.4f;
 
 	// Use this for initialization
 	void Start () {
@@ -27,5 +31,12 @@ public class UnityChanController : MonoBehaviour {
 	void Update () {
 		// Unityちゃんに前方向の力を加える
 		this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
+
+		// Unityちゃんを矢印キーorボタンで左右に移動させる
+		if ((Input.GetKey(KeyCode.LeftArrow)) && -this.movableRange < this.transform.position.x) {
+			this.myRigidbody.AddForce(-this.turnForce, 0, 0);
+		} else if ((Input.GetKey(KeyCode.RightArrow)) && this.transform.position.x < this.movableRange) {
+			this.myRigidbody.AddForce(this.turnForce, 0, 0);
+		}
 	}
 }
