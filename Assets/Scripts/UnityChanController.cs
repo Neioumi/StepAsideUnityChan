b6dea93d7816382f1bc5580cc44d03bc9d15f857
Neioumi <neioumi@me.com>
2017-06-29 20:37:27 +0900

    Unityちゃんがジャンプできるように

--- Assets/Scripts/UnityChanController.cs
+++ Assets/Scripts/UnityChanController.cs
@@ -11,6 +11,8 @@ public class UnityChanController : MonoBehaviour {
 	private float forwardForce = 800.0f;
 	// 左右移動のための力
 	private float turnForce = 500.0f;
+	// ジャンプするための力
+	private float upForce = 500.0f;
 	// 左右移動できる範囲
 	private float movableRange = 3.4f;
 
@@ -33,10 +35,20 @@ public class UnityChanController : MonoBehaviour {
 		this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
 
 		// Unityちゃんを矢印キーorボタンで左右に移動させる
-		if ((Input.GetKey(KeyCode.LeftArrow)) && -this.movableRange < this.transform.position.x) {
+		if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x) {
 			this.myRigidbody.AddForce(-this.turnForce, 0, 0);
-		} else if ((Input.GetKey(KeyCode.RightArrow)) && this.transform.position.x < this.movableRange) {
+		} else if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange) {
 			this.myRigidbody.AddForce(this.turnForce, 0, 0);
+		}
+
+		// Jumpステートの場合、Jumpにfalseをセット
+		if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump")) {
+			this.myAnimator.SetBool("Jump", false);
+		}
+		// ジャンプしていない時にスペースが押されたらジャンプする
+		if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f) {
+			this.myAnimator.SetBool("Jump", true);
+			this.myRigidbody.AddForce(this.transform.up * this.upForce);
 		}
 	}
 }
