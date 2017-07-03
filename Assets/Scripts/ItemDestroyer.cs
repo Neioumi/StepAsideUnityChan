b6c96133930ad1fd16eecabacfa61aa23a893e9a
Neioumi <neioumi@me.com>
2017-07-04 01:13:30 +0900

    ゲームオブジェクトの破棄を別の方法でやるために、まず走っているUnityちゃんの位置を取得

--- Assets/Scripts/ItemDestroyer.cs
+++ Assets/Scripts/ItemDestroyer.cs
@@ -1,21 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemDestroyer : MonoBehaviour {
+	private GameObject unitychan;
+	private float unitychanPos;
+
+	// デバッグ用のテキスト
+	private GameObject debugText;
 
 	// Use this for initialization
 	void Start () {
+		// Unityちゃんのオブジェクトを取得
+		this.unitychan = GameObject.Find("unitychan");
+		// Unityちゃんの位置
+		// this.unitychanPos = unitychan.transform.position.z;
+		// シーン中のdebugTextオブジェクトを取得
+		debugText = GameObject.Find("DebugText");
 		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// Unityちゃんの位置
+		unitychanPos = unitychan.transform.position.z;
+		// Unityちゃんの位置を画面内にテキストで表示
+		debugText.GetComponent<Text>().text = "unitychanPos" + unitychanPos;
 	}
 
-	void OnBecameInvisible() {
-		Debug.Log("Destroy " + this.gameObject);
-		Destroy(this.gameObject);
-	}
+	// レンダラーがカメラから見えなくなった時に呼び出されるので、
+	// コインは消えない（親を作ってレンダラー追加すれば消せる）
+	// void OnBecameInvisible() {
+	// 	Debug.Log("Destroy " + this.gameObject);
+	// 	Destroy(this.gameObject);
+	// }
 }
