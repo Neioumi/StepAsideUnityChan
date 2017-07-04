1fdde313fecb2c708d81cf8638dce49c8f544d37
Neioumi <neioumi@me.com>
2017-07-04 23:38:42 +0900

    デバッグ用テキストをデバッグフラグで表示切り替えできるよう修正

--- Assets/Scripts/ItemDestroyer.cs
+++ Assets/Scripts/ItemDestroyer.cs
@@ -7,6 +7,8 @@ public class ItemDestroyer : MonoBehaviour {
 	private GameObject unitychan;
 	private float unitychanPos;
 
+	// デバッグフラグ
+	private bool debugFlag = false;
 	// デバッグ用のテキスト
 	private GameObject debugText;
 
@@ -17,18 +19,27 @@ public class ItemDestroyer : MonoBehaviour {
 	void Start () {
 		// Unityちゃんのオブジェクトを取得
 		this.unitychan = GameObject.Find("unitychan");
-		// シーン中のdebugTextオブジェクトを取得
-		debugText = GameObject.Find("DebugText");
-		
 
+		// デバッグ時
+		if (debugFlag == true) {
+			// シーン中のdebugTextオブジェクトを取得
+			// SetActive(false)の自身は探せないが、親のtransformから探す
+			debugText = GameObject.Find("Canvas").transform.Find("DebugText").gameObject;
+			// デバッグ用テキストを表示
+			debugText.SetActive(true);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// Unityちゃんの位置
 		unitychanPos = unitychan.transform.position.z;
-		// Unityちゃんの位置を画面内にテキストで表示
-		debugText.GetComponent<Text>().text = "unitychanPos" + unitychanPos;
+
+		// デバッグ時はUnityちゃんの位置を画面内にテキストで表示
+		if (debugFlag == true) {
+			debugText.GetComponent<Text>().text = "unitychanPos" + unitychanPos + "m";
+		}
+		
 		// 各オブジェクトの位置が、Unityちゃんの後ろ、カメラに映らない位置になったら破棄
 		if (this.transform.position.z < unitychanPos - margin) {
 			Destroy(this.gameObject);
